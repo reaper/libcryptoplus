9625d8169a56235f4ac8a29693bf330c4f94405c
Pierre FILSTROFF <pfilstroff@gmail.com>
2013-01-14 22:37:57 +0100

    Small fix and resolv undefined symbol of PKCS5_PBKDF2_HMAC

--- src/pbkdf2.cpp
+++ src/pbkdf2.cpp
@@ -42,14 +42,16 @@
  * \brief PBKDF2 helper functions.
  */
 
-#include "hash/pbkdf2.hpp"
+#include "cryptoplus/hash/pbkdf2.hpp"
 
 #include <openssl/opensslv.h>
 
 #include <stdexcept>
 
-#if OPENSSL_VERSION_NUMBER < 0x10000000
-namespace
+#include <cassert>
+
+#if OPENSSL_VERSION_NUMBER >= 0x10000000
+namespace cryptoplus
 {
 	int PKCS5_PBKDF2_HMAC(const char* pass, int passlen, const unsigned char* salt, int saltlen, int iter, const EVP_MD* md, int outlen, unsigned char* out)
 	{
@@ -63,27 +65,25 @@ namespace
 }
 #endif
 
-#include <cassert>
-
 namespace cryptoplus
 {
-	namespace hash
-	{
-		size_t pbkdf2(const void* password, size_t passwordlen, const void* salt, size_t saltlen, void* outbuf, size_t outbuflen, const message_digest_algorithm& algorithm, unsigned int iter)
-		{
-			int result = PKCS5_PBKDF2_HMAC(
-			                 static_cast<const char*>(password),
-			                 static_cast<int>(passwordlen),
-			                 static_cast<const unsigned char*>(salt),
-			                 static_cast<int>(saltlen),
-			                 static_cast<int>(iter),
-			                 algorithm.raw(),
-			                 static_cast<int>(outbuflen),
-			                 static_cast<unsigned char*>(outbuf)
-			             );
+  namespace hash
+  {
+    size_t pbkdf2(const void* password, size_t passwordlen, const void* salt, size_t saltlen, void* outbuf, size_t outbuflen, const message_digest_algorithm& algorithm, unsigned int iter)
+    {
+      int result = cryptoplus::PKCS5_PBKDF2_HMAC(
+          static_cast<const char*>(password),
+          static_cast<int>(passwordlen),
+          static_cast<const unsigned char*>(salt),
+          static_cast<int>(saltlen),
+          static_cast<int>(iter),
+          algorithm.raw(),
+          static_cast<int>(outbuflen),
+          static_cast<unsigned char*>(outbuf)
+          );
 
-			return result;
-		}
-	}
+      return result;
+    }
+  }
 }
 
